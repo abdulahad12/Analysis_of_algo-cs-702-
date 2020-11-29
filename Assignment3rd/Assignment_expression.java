ee7ee0fb164de2a16a6098ba50248be82a58bb3b
AbdulAhad <abdulahad9691@gmail.com>
2020-11-29 23:30:45 +0500

    third commit

---
+++ Assignment3rd/Assignment_expression.java
@@ -0,0 +1,9 @@
+ public class Assignment_expression{
+  public static void main(String[] args) {
+     int num;                             //declaration of variable
+      num=2;                              // assign value to variable
+     num=num+1;                           //expression 
+      System.out.println(num);
+   	
+  }
+}
